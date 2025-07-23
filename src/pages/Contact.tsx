fe1a15c3ddac87c186a3e6bf43a435c8c307d1ec
Karthikentrepreneur <karthikjungleemara@gmail.com>
2025-07-23 15:31:16 +0530

    Update Contact.tsx

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -18,7 +18,7 @@ const Contact = () => {
     const formData = new FormData(form);
 
     try {
-      const response = await fetch("https://formsubmit.co/ajax/karthikjungleeemara@gmail.com", {
+      const response = await fetch("https://formsubmit.co/ajax/karthikjungleemara@gmail.com", {
         method: "POST",
         headers: {
           Accept: "application/json",
